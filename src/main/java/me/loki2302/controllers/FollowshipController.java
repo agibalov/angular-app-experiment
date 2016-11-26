cc9a715b0e6742fd9cdbdad4e52910fd41500edb
loki2302 <loki2302@loki2302.me>
2016-11-26 17:49:54 -0500

    Added post view

--- src/main/java/me/loki2302/controllers/FollowshipController.java
+++ src/main/java/me/loki2302/controllers/FollowshipController.java
@@ -2,8 +2,10 @@ package me.loki2302.controllers;
 
 import me.loki2302.CommandHandler;
 import me.loki2302.QueryHandler;
+import me.loki2302.commands.CreateFollowshipCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +19,13 @@ public class FollowshipController {
     private QueryHandler queryHandler;
 
     @RequestMapping(value = "/followships", method = RequestMethod.POST)
-    public ResponseEntity createFollowship() {
-        throw new RuntimeException("Not implemented");
+    public ResponseEntity createFollowship(@RequestBody FollowshipAttributesDto followshipAttributesDto) {
+        CreateFollowshipCommand command = new CreateFollowshipCommand(
+                followshipAttributesDto.followerId,
+                followshipAttributesDto.leaderId);
+
+        commandHandler.handle(command);
+
+        return ResponseEntity.noContent().build();
     }
 }
---
+++ src/main/java/me/loki2302/readmodel/postview/PostViewRepository.java
@@ -0,0 +1,5 @@
+package me.loki2302.readmodel.postview;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostViewRepository extends JpaRepository<PostView, Long> {}
